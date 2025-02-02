d2d2f778944d6af87843a0b09924ff78f31d141b
Jupiter Jayna <jupiter@jayna.family>
2025-02-02 20:57:51 +0000

    refactor: Rename Bouwkamp drawing to Simple Perfect Rectangle

--- js/drawings.js
+++ js/drawings.js
@@ -2,7 +2,7 @@ import { DrawingConfig } from './DrawingConfig.js';
 import { colorPalette } from './colorPalette.js';
 
 export const drawings = {
-    bouwkamp5: new DrawingConfig(
+    simplePerfectRectangle: new DrawingConfig(
         [17, 403, 285, 148, 111, 144, 75, 36, 3, 141, 39, 58, 37, 53, 21, 16, 15, 99, 84, 79],
         { colorPalette }
     ),
