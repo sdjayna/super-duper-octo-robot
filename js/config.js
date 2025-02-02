4906b598605e66af7a5a736bbd8d79f19d940029
Jupiter Jayna <jupiter@jayna.family>
2025-02-02 19:59:40 +0000

    refactor: Update line parameters and expand color palette with more nuanced colors

--- js/config.js
+++ js/config.js
@@ -9,20 +9,61 @@ export const config = {
     },
     // Line drawing parameters
     line: {
-        width: 2.5,      // Width of serpentine line in mm
-        spacing: 2.5,    // Spacing between lines in mm
-        strokeWidth: 0.1  // SVG stroke width
+        width: 0.30,      // Width of serpentine line in mm
+        spacing: 1.25,    // Spacing between lines in mm
+        strokeWidth: .45  // SVG stroke width
     }
 };
 
 export const colorPalette = [
-    '#FFA500', // orange
-    '#8B4513', // brown
-    '#FF0000', // red
-    '#FFC0CB', // pink
-    '#800080', // purple
-    '#008000', // green
-    '#0000FF', // blue
-    '#000000', // black
-    '#704214'  // sepia
+    '#fff713', // Zinc Yellow
+    '#fcc253', // Sahara Beige Pastel
+    '#be3218', // Lobster
+    '#d51023', // Traffic Red
+    '#81b5c0', // Lago Blue Pastel
+    '#004470', // Petrol
+    '#56407e', // Currant
+    '#1e1056', // Violet Dark
+    '#ee7620', // DARE Orange
+    '#6a192c', // Burgundy
+    '#391602', // Hazelnut Brown
+    '#00632e', // MISTER Green
+    '#ffeb91', // Vanilla Pastel
+    '#f9a97e', // Peach Pastel
+    '#004811', // Future Green
+    '#ffffff', // Signal White
+    '#00a2e2', // Shock Blue Middle
+    '#000000', // Signal Black
+    '#eb5692', // Neon Pink
+    '#cca9d0', // Lilac Pastel
+    '#66b8eb', // Ceramic Light Pastel
+    '#808a94', // Cool Grey Pastel
+    '#004b9a', // True Blue
+    '#7a9b7a', // Amazonas Light
+    '#009994', // Lagoon Blue
+    '#f8c1b8', // Powder Pastel
+    '#d68307', // Ocher Brown Light
+    '#739ad1', // Blue Violet Pastel
+    '#ff32a6', // Neon Pink Fluorescent
+    '#ff6d1e', // Neon Orange Fluorescent
+    '#79f237', // Neon Green Fluorescent
+    '#fffc00', // Neon Yellow Fluorescent
+    '#b1cc35', // Grasshopper
+    '#49ae41', // KACAO77 Green
+    '#000000', // Metallic Black
+    '#148cc8', // Metallic Blue
+    '#b446aa', // Metallic Pink
+    '#6ea064', // Metallic Light Green
+    '#bebebe', // Metallic Silver
+    '#a59253', // Metallic Gold
+    '#faf4e3', // Nature White
+    '#0083ba', // Shock Blue
+    '#d36aa2', // Fuchsia Pink
+    '#b71d5b', // Magenta
+    '#74324a', // Purple Violet
+    '#74bd8b', // Calypso Middle
+    '#00794d', // Turquoise
+    '#dedd2e', // Poison Green
+    '#bdbcbb', // Blue Grey Light
+    '#6e7172'  // Blue Grey Dark
 ];
